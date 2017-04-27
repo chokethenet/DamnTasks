403d4539f72af9136a1c2a9342bf9e4f500f03b7
ch0ke <choke84@gmail.com>
2017-04-27 16:54:37 +0200

    Add menu and about activity

--- app/src/main/java/net/chokethe/damntasks/MainActivity.java
+++ app/src/main/java/net/chokethe/damntasks/MainActivity.java
@@ -1,7 +1,13 @@
 package net.chokethe.damntasks;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import net.chokethe.damntasks.about.AboutActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                Intent startAboutActivity = new Intent(this, AboutActivity.class);
+                startActivity(startAboutActivity);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/net/chokethe/damntasks/about/AboutActivity.java
@@ -0,0 +1,74 @@
+package net.chokethe.damntasks.about;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import net.chokethe.damntasks.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String URI_MAILTO = "mailto:";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        findViewById(R.id.about_iv_mail_logo).setOnClickListener(this);
+        findViewById(R.id.about_tv_mail_uri).setOnClickListener(this);
+        findViewById(R.id.about_iv_github_logo).setOnClickListener(this);
+        findViewById(R.id.about_tv_github_uri).setOnClickListener(this);
+
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+
+            TextView versionNameTextView = (TextView) findViewById(R.id.about_tv_version_name);
+            String versionName = "v" + pInfo.versionName;
+            versionNameTextView.setText(versionName);
+
+            TextView sinceTextView = (TextView) findViewById(R.id.about_tv_since);
+            SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+            sinceTextView.setText(dt.format(new Date(pInfo.lastUpdateTime)));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.about_iv_mail_logo:
+            case R.id.about_tv_mail_uri:
+                intentMail();
+                break;
+            case R.id.about_iv_github_logo:
+            case R.id.about_tv_github_uri:
+                intentBrowser();
+                break;
+        }
+    }
+
+    private void intentMail() {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse(URI_MAILTO));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{getString(R.string.about_mail_to)});
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.about_mail_subject));
+        startActivity(intent);
+    }
+
+    private void intentBrowser() {
+        Uri uri = Uri.parse(getString(R.string.about_github_uri));
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+}
